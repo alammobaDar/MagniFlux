bc486a977de4c63bdc04f6f4df87a4e578e16c2c
AlammobaDar <162420273+alammobaDar@users.noreply.github.com>
2025-05-29 21:13:22 +0800

    configure endpoints and models

--- Backend/app/model.py
+++ Backend/app/model.py
@@ -1,7 +1,23 @@
 from pydantic import BaseModel
 
 
+
 class InputModel(BaseModel):
     magnetic_field: float
     area: float
     theta: float
+
+class TextModel(BaseModel):
+    text: str
+
+    # @validator("magnetic_field", "area")
+    # def must_be_positive(cls, v):
+    #     if v < 0:
+    #         raise ValueError("Value must be non-negative.")
+    #     return v
+
+    # @validator("theta")
+    # def must_be_angle(cls, v):
+    #     if not 0 <= v <= 360:
+    #         raise ValueError("Angle must be between 0 and 360 degrees.")
+    #     return v
