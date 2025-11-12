b173150e15e7c0dfa28ab57a89c03bc0f9df6fee
AlammobaDar <162420273+alammobaDar@users.noreply.github.com>
2025-11-12 22:54:38 +0800

    change server url path

--- Frontend/app/src/utils/utils.jsx
+++ Frontend/app/src/utils/utils.jsx
@@ -1,5 +1,5 @@
 export async function SendInputsForExplanation(newPost) {
-    const response = await fetch("http://127.0.0.1:8000/api/explain/", {
+    const response = await fetch("https://magni-flux-backend.vercel.app/api/explain/", {
                 method: 'POST',
                 headers:{'Content-type': 'application/json'},
                 body: JSON.stringify(newPost),
@@ -11,7 +11,7 @@ export async function SendInputsForExplanation(newPost) {
 }
 
 export async function SendText(newPost) {
-    const response = await fetch("http://127.0.0.1:8000/api/ai/", {
+    const response = await fetch("https://magni-flux-backend.vercel.app/api/ai/", {
         method: 'POST',
         headers:{'Content-type': 'application/json'},
         body: JSON.stringify(newPost),
